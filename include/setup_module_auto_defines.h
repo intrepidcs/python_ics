4d52ffda9c1edd9707f363921174d24d441f4907
David Rebbe <drebbe@intrepidcs.com>
2017-03-01 20:50:30 -0500

    including these file temporarily for readthedocs
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

---
+++ include/setup_module_auto_defines.h
@@ -0,0 +1,7 @@
+#ifndef __SETUP_MODULE_AUTO_DEFINES_H__
+#define __SETUP_MODULE_AUTO_DEFINES_H__
+#include <Python.h>
+
+int setup_module_auto_defines(PyObject * module);
+
+#endif // __SETUP_MODULE_AUTO_DEFINES_H__
