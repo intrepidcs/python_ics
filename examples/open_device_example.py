a71acbf49eb9e436efc3a185a8f6e2d3bd00068a
David Rebbe <drebbe@intrepidcs.com>
2018-08-07 10:39:53 -0400

    Added two examples.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

---
+++ examples/open_device_example.py
@@ -0,0 +1,40 @@
+import ics
+
+# Tells our custom open_device() function to do special behavior
+# with neoVI Server enabled so we can successfully open devices
+enable_use_server = True
+
+def dev_name(device):
+    # Return a friendly name of the device (ie. neoVI FIRE2 CY1234)
+    if int("AA0000", 36) <= device.SerialNumber <= int("ZZZZZZ", 36):
+        return device.Name + " " + ics.base36enc(device.SerialNumber)
+    else:
+        return device.Name + " " + str(device.SerialNumber)
+        
+def open_device(index=0):
+    device = None
+    if enable_use_server:
+        # ics.open_device() won't open a device if we have handles open already
+        # so we need to find them and specify which ones to connect to.
+        devices = ics.find_devices()
+        print("Opening Device {} (Open Client handles: {})...".format(dev_name(devices[index]), devices[index].NumberOfClients))
+        ics.open_device(devices[index])
+        device = devices[index]
+    else:
+        print("Opening Device...")
+        device = ics.open_device()
+    print("Opened Device %s." % dev_name(device))
+    return device
+    
+    
+if __name__ == "__main__":
+    # Lets figure out how many are connected to the PC and display it
+    connected_count = len(ics.find_devices())
+    print("Found {} connected device(s)...".format(connected_count))
+    for i in range(connected_count):
+        # Lets open the device
+        device = open_device(i)
+        # the device will automatically close here
+        # because the device object is going out of
+        # scope.
+    print("Finished.")
