59185aaf2850f83bbe697e571cef4c62367c789c
David Rebbe <drebbe@intrepidcs.com>
2019-02-19 11:27:58 -0500

    Fat fingered the keyboard... version 2.16 this time.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sys
 import unittest
 
 MAJOR_VERSION = 2
-MINOR_VERSION = 17
+MINOR_VERSION = 16
 
 def _run_tests():
     directory = os.path.abspath(os.path.dirname(sys.modules['__main__'].__file__))
