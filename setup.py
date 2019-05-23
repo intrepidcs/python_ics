7ff0809f117ac291233240a5f23368de49544a4d
David Rebbe <drebbe@intrepidcs.com>
2019-05-23 14:07:25 -0400

    Removed Python 2 support from build script. Fixes issue #44.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- setup.py
+++ setup.py
@@ -94,8 +94,6 @@ setup (name = 'python_ics',
        download_url = 'https://github.com/intrepidcs/python_ics/releases',
        ext_modules = [module],
        classifiers = [
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
