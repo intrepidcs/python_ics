a23eaac6e050610504e0e9b35aa346abd65c66a1
Bryant Jones <bjones@intrepidcs.com>
2020-02-26 17:50:52 -0500

    Temporary fix for improper parsing on op_eth structures with unions

--- ics/structures/op_eth_general_settings.py
+++ ics/structures/op_eth_general_settings.py
@@ -5,10 +5,20 @@ import ctypes
 class flags(ctypes.Structure):
     _pack_ = 2
     _fields_ = [
+        ('bTapEnSwitch', ctypes.c_uint32, 1), # [Bitfield] 
+        ('bTapEnPtp', ctypes.c_uint32, 1), # [Bitfield] 
+        ('bEnReportLinkQuality', ctypes.c_uint32, 1), # [Bitfield] 
     ]
 
 # Extra names go here:
 # End of extra names
+
+class op_eth_general_settings_u1(ctypes.Union):
+    _pack_ = 2
+    _fields_ = [
+        ('flags', flags), 
+        ('uFlags', ctypes.c_uint32), 
+    ]
 
 class op_eth_general_settings(ctypes.Structure):
     _pack_ = 2
@@ -21,11 +31,7 @@ class op_eth_general_settings(ctypes.Structure):
         ('tapPair3', ctypes.c_uint16), 
         ('tapPair4', ctypes.c_uint16), 
         ('tapPair5', ctypes.c_uint16), 
-        ('flags', flags), 
-        ('bTapEnSwitch', ctypes.c_uint32, 1), # [Bitfield] 
-        ('bTapEnPtp', ctypes.c_uint32, 1), # [Bitfield] 
-        ('bEnReportLinkQuality', ctypes.c_uint32, 1), # [Bitfield] 
-        ('uFlags', ctypes.c_uint32), 
+        ('u1', op_eth_general_settings_u1), 
     ]
 
 # Extra names go here:
