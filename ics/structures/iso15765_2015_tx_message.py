ffed7cf9b78b489e65388eb8b23e4d43c64fb0a9
David Rebbe <drebbe@intrepidcs.com>
2021-04-03 23:28:39 -0400

    Fixed build issues under linux.

--- ics/structures/iso15765_2015_tx_message.py
+++ ics/structures/iso15765_2015_tx_message.py
@@ -36,7 +36,7 @@ class iso15765_2015_tx_message(ctypes.Structure):
         ('extendedAddress', ctypes.c_uint8), 
         ('fs_timeout', ctypes.c_uint16), 
         ('fs_wait', ctypes.c_uint16), 
-        ('data', ctypes.POINTER(ctypes.c_uint8 )), 
+        ('data', ctypes.POINTER(ctypes.c_uint8)), 
         ('num_bytes', ctypes.c_uint32), 
         ('tx_dl', ctypes.c_uint8), 
         ('_U7', _U7), 
