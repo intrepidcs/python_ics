98865d9929e2d31ae0649e7030a99666725d2ef1
David Rebbe <drebbe@intrepidcs.com>
2021-04-05 15:55:12 -0400

    Updated Python versions.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- setup.py
+++ setup.py
@@ -101,9 +101,6 @@ setup (name = 'python_ics',
        
        classifiers = [
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
