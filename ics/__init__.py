90fd6cd9558d25022780f83407b105e4d2b2be47
David Rebbe <drebbe@intrepidcs.com>
2021-06-08 16:56:07 -0400

    Update init to include hidden_imports.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- ics/__init__.py
+++ ics/__init__.py
@@ -1,6 +1,8 @@
 try:
     from ics import *
     from structures import *
+    from hiddenimports import hidden_imports
 except:
     from ics.ics import *
     from ics.structures import *
+    from ics.hiddenimports import hidden_imports
