62042c114820269145a2b57638a53ea7b1aca3c8
David Rebbe <drebbe@intrepidcs.com>
2021-07-19 11:07:13 -0400

    Fixed imports

--- ics/__init__.py
+++ ics/__init__.py
@@ -1,8 +1,12 @@
+
+from ics.structures import *
+
 try:
     from ics import *
-    from structures import *
-    from hiddenimports import hidden_imports
-except:
-    from ics.ics import *
     from ics.structures import *
     from ics.hiddenimports import hidden_imports
+except Exception as ex:
+    print(ex)
+    from ics.ics import *
+    from ics.ics.structures import *
+    from ics.ics.hiddenimports import hidden_imports
