cd90fda5378d572ee07bd3f8ff16a6b94bab5870
David Rebbe <drebbe@intrepidcs.com>
2021-07-21 14:33:26 -0400

    Fixed imports.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- ics/__init__.py
+++ ics/__init__.py
@@ -3,10 +3,12 @@ from ics.structures import *
 
 try:
     from ics import *
+    from ics.ics import *
     from ics.structures import *
     from ics.hiddenimports import hidden_imports
 except Exception as ex:
     print(ex)
     from ics.ics import *
+    from ics.ics.ics import *
     from ics.ics.structures import *
     from ics.ics.hiddenimports import hidden_imports
