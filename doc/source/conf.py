e5cf0ab44e7c3e8d61b7e7d0cdeb95f388f6be40
David Rebbe <drebbe@intrepidcs.com>
2022-01-10 15:49:02 -0500

    updated copyright
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ics'
-copyright = '2021, Intrepid Control Systems, Inc.'
+copyright = '2022, Intrepid Control Systems, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
