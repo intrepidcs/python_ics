ba80f1b5fd39e660ea01381471add1aa416cae3e
David Rebbe <drebbe@intrepidcs.com>
2022-03-15 15:13:14 -0400

    Added mac build script.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

---
+++ build_mac.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+python_directory="/Library/Frameworks/Python.framework/Versions"
+python_versions=("3.7" "3.8" "3.9" "3.10")
+
+# libusb dependencies
+brew install automake libtool cmake clang-format
+ret_value=$?
+if [ $ret_value -ne 0 ]; then
+    echo "Failed to install automake/libtoolgit..."
+    exit $ret_value
+fi
+
+# Stash any changes we don't have committed.
+git stash
+ret_value=$?
+if [ $ret_value -ne 0 ]; then
+    echo "Failed to stash changes through git..."
+    exit $ret_value
+fi
+
+# Make sure all the python versions are installed
+for version in "${python_versions[@]}"
+do
+    python_exec="${python_directory}/${version}/bin/python3"
+    # echo $python_exec
+    $python_exec --version
+    echo "Installing python dependencies..."
+    $python_exec -m pip install wheel
+    ret_value=$?
+    if [ $ret_value -ne 0 ]; then
+        echo "Python requirements for ${version} failed!"
+        exit $ret_value
+    fi
+    echo "Building python ${version}..."
+    $python_exec setup.py bdist_wheel --universal
+    ret_value=$?
+    if [ $ret_value -ne 0 ]; then
+        echo "Builing python_ics module with Python ${version} failed!"
+        exit $ret_value
+    fi
+done
