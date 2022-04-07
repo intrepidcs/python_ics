6dc42c64d013efe082c62284f095be9e179f2db9
David Rebbe <drebbe@intrepidcs.com>
2022-04-07 17:16:19 -0400

    Fixed setup.py to call extract the correct way.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ if POST_VERSION:
 else:
     VERSION_STRING = '%d.%d' % (MAJOR_VERSION, MINOR_VERSION)
 
+
 def _run_tests():
     directory = os.path.abspath(os.path.dirname(sys.modules['__main__'].__file__))
     loader = unittest.defaultTestLoader
@@ -24,37 +25,39 @@ def _run_tests():
     suite = loader.discover(os.path.join(directory, 'test'))
     runner.run(suite)
 
+
 try:
     from setuptools.command.test import test
-    
-    
+
     class UnitTests(test):
         def finalize_options(self):
             test.finalize_options(self)
             self.test_args = []
             self.test_suite = True
-        
+
         def run_tests(self):
             _run_tests()
 except ImportError:
     from distutils.core import Command
-    
+
     class UnitTests(Command):
         user_options = []
+
         def initialize_options(self):
-                pass
-                
+            pass
+
         def finalize_options(self):
             pass
-        
+
         def run(self):
             _run_tests()
 
 
 class build(build_module.build):
     def run(self):
-        import extract_icsneo40_defines # there should be a better way to do this...
+        import extract_icsneo40_defines
         import generate_icsneo40_structs
+        extract_icsneo40_defines.extract()
         generate_icsneo40_structs.generate()
         if 'DARWIN' in platform.system().upper():
             import build_libicsneo
@@ -62,6 +65,7 @@ class build(build_module.build):
             build_libicsneo.build()
             build_libicsneo.copy()
         build_module.build.run(self)
+
 
 home_path = os.path.expanduser('~')
 # Grab all the source files
@@ -74,7 +78,7 @@ for root, dirs, files in os.walk('src'):
 # Set compiler flags here
 if 'LINUX' in platform.system().upper():
     compile_args = [
-        '-fpermissive', 
+        '-fpermissive',
         '-Wno-unused-variable',
         '-Wno-unused-function',
         '-Wno-write-strings'
@@ -91,39 +95,42 @@ if not shutil.which('clang-format'):
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    
-ics_extension = Extension('ics.ics',
-  define_macros = [('MAJOR_VERSION', MAJOR_VERSION), ('MINOR_VERSION', MINOR_VERSION), ('EXTERNAL_PROJECT', 1)],
-  include_dirs=['include', 'include/ics', 'include/ice'],
-  libraries = [],
-  library_dirs = ['/usr/local/lib'],
-  sources = source_files,
-  extra_compile_args=compile_args)
+
+
+ics_extension = Extension(
+    'ics.ics',
+    define_macros=[('MAJOR_VERSION', MAJOR_VERSION),
+                   ('MINOR_VERSION', MINOR_VERSION),
+                   ('EXTERNAL_PROJECT', 1)],
+    include_dirs=['include', 'include/ics', 'include/ice'],
+    libraries=[],
+    library_dirs=['/usr/local/lib'],
+    sources=source_files, extra_compile_args=compile_args)
 
 package_data = {}
 if 'DARWIN' in platform.system().upper():
     package_data['ics'] = ['*.dylib']
 
-setup (name = 'python_ics',
-       version = VERSION_STRING,
-       description = 'Library for interfacing with Intrepid devices in Python',
-       long_description = read('README.md'),
-       license = "MIT",
-       author = 'David Rebbe',
-       author_email='drebbe@intrepidcs.com',
-       maintainer = 'David Rebbe',
-       maintainer_email='drebbe@intrepidcs.com',
-       url='https://github.com/intrepidcs/python_ics/',
-       cmdclass = { 'build': build, 'test': UnitTests, },
-       download_url = 'https://github.com/intrepidcs/python_ics/releases',
-       packages = ['ics', 'ics.structures'],
-       package_data = package_data,
-       include_package_data = True,
-       ext_modules = [ics_extension],
-       classifiers = [
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        ],)
+setup(name='python_ics',
+      version=VERSION_STRING,
+      description='Library for interfacing with Intrepid devices in Python',
+      long_description=read('README.md'),
+      license="MIT",
+      author='David Rebbe',
+      author_email='drebbe@intrepidcs.com',
+      maintainer='David Rebbe',
+      maintainer_email='drebbe@intrepidcs.com',
+      url='https://github.com/intrepidcs/python_ics/',
+      cmdclass={'build': build, 'test': UnitTests, },
+      download_url='https://github.com/intrepidcs/python_ics/releases',
+      packages=['ics', 'ics.structures'],
+      package_data=package_data,
+      include_package_data=True,
+      ext_modules=[ics_extension],
+      classifiers=[
+           'Programming Language :: Python :: 3',
+           'Programming Language :: Python :: 3.6',
+           'Programming Language :: Python :: 3.7',
+           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+      ],)
