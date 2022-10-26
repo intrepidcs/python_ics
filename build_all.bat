a011898e1ffc1e9d209d29dd6b306e4701bef2d0
David Rebbe <drebbe@intrepidcs.com>
2022-10-26 09:28:10 -0400

    Adding python 3.11 to the build script.

--- build_all.bat
+++ build_all.bat
@@ -11,23 +11,28 @@ C:\Python\Python37-32\scripts\pip install wheel twine --upgrade
 C:\Python\Python38-32\scripts\pip install wheel twine --upgrade
 C:\Python\Python39-32\scripts\pip install wheel twine --upgrade
 C:\Python\Python310-32\scripts\pip install wheel twine --upgrade
+C:\Python\Python311-32\scripts\pip install wheel twine --upgrade
 
 C:\Python\Python36-64\scripts\pip install wheel twine --upgrade
 C:\Python\Python37-64\scripts\pip install wheel twine --upgrade
 C:\Python\Python38-64\scripts\pip install wheel twine --upgrade
 C:\Python\Python39-64\scripts\pip install wheel twine --upgrade
 C:\Python\Python310-64\scripts\pip install wheel twine --upgrade
+C:\Python\Python311-64\scripts\pip install wheel twine --upgrade
 
 C:\Python\Python36-32\python setup.py sdist bdist_wheel
 C:\Python\Python37-32\python setup.py sdist bdist_wheel
 C:\Python\Python38-32\python setup.py sdist bdist_wheel
 C:\Python\Python39-32\python setup.py sdist bdist_wheel
 C:\Python\Python310-32\python setup.py sdist bdist_wheel
+C:\Python\Python311-32\python setup.py sdist bdist_wheel
 
 C:\Python\Python36-64\python setup.py sdist bdist_wheel
 C:\Python\Python37-64\python setup.py sdist bdist_wheel
 C:\Python\Python38-64\python setup.py sdist bdist_wheel
 C:\Python\Python39-64\python setup.py sdist bdist_wheel
 C:\Python\Python310-64\python setup.py sdist bdist_wheel
+C:\Python\Python311-64\python setup.py sdist bdist_wheel
+
 
 REM C:\Python\Python37-32\scripts\twine upload dist/* -r pypitest
