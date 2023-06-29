69dcd401a72aa95cfb504d12bf38f5d7aa4f0a30
David Rebbe <drebbe@intrepidcs.com>
2023-06-29 09:18:56 -0400

    Updated libicsneo hash to fix CANFD length issue. #150
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- build_libicsneo.py
+++ build_libicsneo.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import sys
 
-LIBICSNEO_COMMIT = '5b3e21fa7ca8dfa2c61105d8f36a0638eb177dca'
+LIBICSNEO_COMMIT = '9ef93eb73e1ada72ccc7a571744b9db1a564f18b'
 LIBUSB_COMMIT = '4239bc3a50014b8e6a5a2a59df1fff3b7469543b'
 CPUS = str(multiprocessing.cpu_count())
 
