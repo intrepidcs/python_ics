05b40720bec2d1056a7db659161e17b33bbbefc0
David Rebbe <drebbe@intrepidcs.com>
2024-02-23 11:22:07 -0500

    unit test start

---
+++ tests/runner/Readme.md
@@ -0,0 +1,14 @@
+Runner tests
+===
+
+This directory is specifically for testing against hardware attached to the runner.
+
+Each runner consists of the following hardware:
+
+- neoVI FIRE3
+- neoVI FIRE2
+- ValueCAN4-2
+
+
+- RAD-Moon2
+- RAD-Moon2
---
+++ tests/runner/test_open_close.py
@@ -0,0 +1,34 @@
+import unittest
+import ics
+
+
+class TestOpenClose(unittest.TestCase):
+    def setUp(self):
+        expected_dev_count = 3
+        self.devices = ics.find_devices()
+        self.assertEqual(len(self.devices), expected_dev_count, f"Expected {expected_dev_count}, found {len(self.devices)}...")
+
+    def test_find_fire3(self):
+        devices = ics.find_devices(ics.NEODEVICE_FIRE3)
+        self.assertTrue(len(devices) == 1)
+
+    def test_find_fire2(self):
+        devices = ics.find_devices(ics.NEODEVICE_FIRE2)
+        self.assertTrue(len(devices) == 1)
+
+    def test_find_vcan42(self):
+        devices = ics.find_devices(ics.NEODEVICE_VCAN42)
+        self.assertTrue(len(devices) == 1)
+
+    def test_find_fire2_and_vcan42(self):
+        devices = ics.find_devices(ics.NEODEVICE_FIRE2 | ics.NEODEVICE_VCAN42)
+        self.assertTrue(len(devices) == 2)
+    
+    def test_open_close(self):
+        for device in self.devices:
+            d = ics.open_device(device)
+            self.assertEquals(device, d)
+            ics.close_device(d)
+
+if __name__ == "__main__":
+    unittest.main()
