c59042a136be43435d4b6ecb47b084db3cb97590
David Rebbe <drebbe@intrepidcs.com>
2024-03-06 11:09:07 -0500

    debug test

--- src/icsdebug.py
+++ src/icsdebug.py
@@ -1,9 +1,15 @@
 import ics
 
+def show_clients():
+    for d in ics.find_devices():
+        print(d, d.NumberOfClients)
+
 print("Find...")
 devices = ics.find_devices()
+show_clients()
 print("Devices: ", devices)
 d = ics.open_device(devices[0])
+show_clients()
 print("Opened", d)
 print("Loading default settings...", d)
 ics.load_default_settings(d)
