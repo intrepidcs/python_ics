64a2238d1749deab343f347c83ae502dfea5fa47
David Rebbe <drebbe@intrepidcs.com>
2024-03-11 12:12:05 -0400

    add device settings type check
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- tests/runner/test_settings.py
+++ tests/runner/test_settings.py
@@ -1,6 +1,7 @@
 import unittest
 import ics
 from ics.py_neo_device_ex import PyNeoDeviceEx
+from ics.structures.e_device_settings_type import e_device_settings_type
 
 unittest.TestLoader.sortTestMethodsUsing = None
 
@@ -12,11 +13,14 @@ class BaseTests:
         @classmethod
         def setUpClass(cls):
             cls.device_type = None
+            cls.device_settings_type = None
 
         def _get_device(self) -> PyNeoDeviceEx:
             devices = ics.find_devices([self.device_type])
             self.assertEqual(
-                len(devices), 1, f"Failed to find any device types of {self.device_type}! Expected 1, got {len(devices)}."
+                len(devices),
+                1,
+                f"Failed to find any device types of {self.device_type}! Expected 1, got {len(devices)}.",
             )
             return devices[0]
 
@@ -34,8 +38,9 @@ class BaseTests:
             try:
                 ics.load_default_settings(device)
                 settings = ics.get_device_settings(device)
+                self.assertEqual(settings.DeviceSettingType, self.device_settings_type)
                 ics.set_device_settings(device, settings)
-                read_back_settings = ics.get_device_settings(device)
+                _ = ics.get_device_settings(device)
                 # TODO: compare both settings
             finally:
                 device.close()
@@ -45,18 +50,21 @@ class TestFire3Settings(BaseTests.TestSettings):
     @classmethod
     def setUpClass(cls):
         cls.device_type = ics.NEODEVICE_FIRE3
+        cls.device_settings_type = e_device_settings_type.DeviceFire3SettingsType
 
 
 class TestFire2Settings(BaseTests.TestSettings):
     @classmethod
     def setUpClass(cls):
         cls.device_type = ics.NEODEVICE_FIRE2
+        cls.device_settings_type = e_device_settings_type.DeviceFire2SettingsType
 
 
 class TestValueCAN42Settings(BaseTests.TestSettings):
     @classmethod
     def setUpClass(cls):
         cls.device_type = ics.NEODEVICE_VCAN42
+        cls.device_settings_type = e_device_settings_type.DeviceVCAN42SettingsType
 
 
 if __name__ == "__main__":
