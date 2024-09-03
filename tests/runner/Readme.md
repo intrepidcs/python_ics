1e7c10abe453adf1a7bc61c487973fee957acc1b
Nick <ncejka@intrepidcs.com>
2024-09-03 18:10:42 -0400

    added to runner readme; improved settings test

--- tests/runner/Readme.md
+++ tests/runner/Readme.md
@@ -3,12 +3,26 @@ Runner tests
 
 This directory is specifically for testing against hardware attached to the runner.
 
-Each runner consists of the following hardware:
-
+Each runner consists of the following ICS hardware:
 - neoVI FIRE3
 - neoVI FIRE2
 - ValueCAN4-2
-
-
 - RAD-Moon2
 - RAD-Moon2
+
+Runner platforms:
+- Windows 10
+- MAC
+- Linux
+- Linux ARM (Raspberry PI)
+
+Hardware setup:
+PC -> USB -> NIC -> ETH -> Fire3 -> CAN/LIN/ETH network v v
+          -> Fire2 -> CAN/LIN/ETH network             v | ^
+          -> VCAN42 -> (2x)CAN network                ^ ^
+          -> NIC -> ETH -> Moon2 -> Moon2 network v
+                        -> Moon2 -> Moon2 network ^
+
+Hardware / software testing:
+- Run all python ics functions on all hardware
+- Verify all USB, ETH, CAN, LIN networks
--- tests/runner/test_settings.py
+++ tests/runner/test_settings.py
@@ -18,11 +18,11 @@ class BaseTests:
         
         def _get_device(self):
             devices = ics.find_devices([self.device_type])
-            # self.assertEqual(
-            #     len(devices),
-            #     1,
-            #     f"Failed to find any device types of {self.device_type}! Expected 1, got {len(devices)}.",
-            # )
+            self.assertEqual(
+                len(devices),
+                self.num_devices,
+                f"Failed to find correct number of devices of type {self.device_type}! Expected {self.num_devices}, got {len(devices)}.",
+            )
             return devices[0]
         
         def _get_device_specific_settings(self, base_settings):
@@ -46,39 +46,56 @@ class BaseTests:
                 ics.load_default_settings(device)
                 time.sleep(sleep_time)
                 base_settings = ics.get_device_settings(device)
-                self.assertEqual(base_settings.DeviceSettingType, self.device_settings_type)
-                # change HSCAN1 term setting and verify
+                self.assertEqual(base_settings.DeviceSettingType, self.device_settings_type, f"Wrong settings type on {device}")
+                # change HSCAN1 term setting and verify (other stuff if Moon2)
                 device_settings = self._get_device_specific_settings(base_settings)
-                device_settings.termination_enables = 0x1
-                device_settings.canfd1.FDBaudrate = ics.BPS1000
+                if self.device_type != 5:
+                    device_settings.canfd1.FDBaudrate = ics.BPS1000  # change CAN1 baudrate
+                else:
+                    device_settings.opEth1.link_spd = 0x1  # change ETH1 speed
                 ics.set_device_settings(device, base_settings)
                 time.sleep(sleep_time)
                 verify_new_settings = self._get_device_specific_settings(ics.get_device_settings(device))
-                self.assertEqual(verify_new_settings.termination_enables, 0x1)
-                self.assertEqual(verify_new_settings.canfd1.FDBaudrate, ics.BPS1000)
+                if self.device_type != 5:
+                    self.assertEqual(verify_new_settings.canfd1.FDBaudrate, ics.BPS1000, f"Failed to set settings on {device}")
+                else:
+                    self.assertEqual(verify_new_settings.opEth1.link_spd, 0x1, f"Failed to set settings on {device}")
                 # verify setting default
                 ics.load_default_settings(device)
                 time.sleep(sleep_time)
                 verify_default_settings = self._get_device_specific_settings(ics.get_device_settings(device))
-                self.assertEqual(verify_default_settings.termination_enables, 0x0)
-                self.assertEqual(verify_default_settings.canfd1.FDBaudrate, ics.BPS2000)
+                if self.device_type != 5:
+                    self.assertEqual(verify_default_settings.canfd1.FDBaudrate, ics.BPS2000, f"Failed to load default settings on {device}")
+                else:
+                    self.assertEqual(verify_default_settings.opEth1.link_spd, 0x2, f"Failed to load default settings on {device}")
             finally:
                 ics.close_device(device)
 
+class TestRADMoon2Settings(BaseTests.TestSettings):
+    @classmethod
+    def setUpClass(cls):
+        cls.device_type = ics.NEODEVICE_RADMOON2
+        cls.device_settings_type = e_device_settings_type.DeviceRADMoon2SettingsType
+        cls.num_devices = 2
+        print("DEBUG: Testing MOON2s...")
 
-
+# HAVING ISSUES SETTING SETTINGS WITH THIS UNIT!
 # class TestFire3Settings(BaseTests.TestSettings):
 #     @classmethod
 #     def setUpClass(cls):
 #         cls.device_type = ics.NEODEVICE_FIRE3
 #         cls.device_settings_type = e_device_settings_type.DeviceFire3SettingsType
+#         cls.num_devices = 1
+#         print("DEBUG: Testing FIRE3...")
 
-
+# ISSUES CONNECTING TO THIS DEVICE AT ALL!!!
 # class TestFire2Settings(BaseTests.TestSettings):
 #     @classmethod
 #     def setUpClass(cls):
 #         cls.device_type = ics.NEODEVICE_FIRE2
 #         cls.device_settings_type = e_device_settings_type.DeviceFire2SettingsType
+#         cls.num_devices = 1
+#         print("DEBUG: Testing FIRE2...")
 
 
 class TestValueCAN42Settings(BaseTests.TestSettings):
@@ -86,13 +103,8 @@ class TestValueCAN42Settings(BaseTests.TestSettings):
     def setUpClass(cls):
         cls.device_type = ics.NEODEVICE_VCAN42
         cls.device_settings_type = e_device_settings_type.DeviceVCAN412SettingsType
-
-
-# class TestRADMoon2Settings(BaseTests.TestSettings):
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.device_type = ics.NEODEVICE_RADMOON2
-#         cls.device_settings_type = e_device_settings_type.DeviceRADMoon2SettingsType
+        cls.num_devices = 1
+        print("DEBUG: Testing VCAN42...")
 
 
 if __name__ == "__main__":
